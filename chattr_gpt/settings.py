c13a50d3a61c2a00024abdbee94bd8e26abd463f
Ryan Schuetzler <ryan@schuetzler.net>
2023-07-25 16:37:43 -0600

    Update vite dir

--- chattr_gpt/settings.py
+++ chattr_gpt/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-VITE_APP_DIR = os.path.join(BASE_DIR, "frontend", "src")
+VITE_APP_DIR = os.path.join(BASE_DIR, "frontend")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
