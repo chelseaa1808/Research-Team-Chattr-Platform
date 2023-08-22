30ba731a26b5e8d8b6607af95a663753bc05f79e
Ryan Schuetzler <ryan@schuetzler.net>
2023-08-22 10:30:35 -0600

    Fix build command base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {manifest: true},
-  base: process.env.mode === 'production' ? '/static/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/static/" : "/",
+  build: {
+    manifest: true,
+  },
   plugins: [react()],
-})
+}));
