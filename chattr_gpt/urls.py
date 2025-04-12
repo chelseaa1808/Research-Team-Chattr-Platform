ed0c4f98c22dbfc07c9ead6d1341b725e5780ef0
chelseaa1808 <cta1@usf.edu>
2025-04-12 17:16:58 -0400

    Update urls.py

--- chattr_gpt/urls.py
+++ chattr_gpt/urls.py
@@ -1,35 +1,20 @@
-"""chattr_lab URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-
-# from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
 
 frontend_urls = [
-    re_path(
-        r"^.*$", TemplateView.as_view(template_name="frontend/index.html"), name="home"
-    ),
+    re_path(r"^.*$", TemplateView.as_view(template_name="frontend/index.html"), name="home"),
 ]
 
 urlpatterns = [
-    path("dQw4w9WgXcQ/", admin.site.urls),
+    path("adminpanel/", admin.site.urls),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/", include("chat.urls")),
     path('accounts/', include('allauth.urls')),
-    # path("auth/", include("authentication.urls")),
 ] + frontend_urls
+
+# Add this line for serving static files in production
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
